f5b7286b3ef1d238ad03b5428a2c99d0140d201d
David-B-A <dabelloa@unal.edu.co>
2018-05-03 16:29:19 -0500

    Adecuando todas las vistas a la plantilla determinada (algunas estaban antes en fondo gris y letras grises por lo que no se veían bien)

--- resources/views/auth/editRol.blade.php
+++ resources/views/auth/editRol.blade.php
@@ -5,61 +5,66 @@
 <div class="row">
     <div class="col-md-2"></div>
     <div class="col-md-8">
+        <div class="panel panel-default">
+     		<div class="panel-heading">
+                Editar roles del usuario <b> {{ $usuario->name }} - {{ $usuario->email }} </b>
+            </div>
 
-        <h4>Usuario - {{ $usuario->name }}</h4>
-        <h4>Email - {{ $usuario->email }}</h4>
+            <div class="panel-body">
 
-        <form  method="POST" action="{{  url('usuario/editRol').'/'.$usuario->id  }}">
-            <input type="hidden" name="_method" value="PUT">
-            {{ csrf_field() }}
-           <table class="table">
-                <thead>
-                    <tr>
-                        <th>Rol</th>
-                        <th>Activo</th>
-                        <th>Desactivo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Administrador</td>
-                        @if ($rol1 == 1)
-                        <td><input type="radio" value="1" name="estadoRol1" checked="true"></td>
-                        <td><input type="radio" name="estadoRol1" value="0"></td>
-                        @else
-                        <td><input type="radio" value="1" name="estadoRol1"></td>
-                        <td><input type="radio" name="estadoRol1" value="0" checked="true"></td>   
-                        @endif  
-                    </tr>
-                    <tr>
-                        <td>Arrendatario</td>
-                        @if ($rol2 == 1)
-                        <td><input type="radio" value="1" name="estadoRol2" checked="true"></td>
-                        <td><input type="radio" name="estadoRol2" value="0"></td>
-                        @else
-                        <td><input type="radio" value="1" name="estadoRol2"></td>
-                        <td><input type="radio" name="estadoRol2" value="0" checked="true"></td>   
-                        @endif  
-                    </tr>
-                    <tr>
-                         <td>Propietario</td>
-                         @if ($rol3 == 1)
-                         <td><input type="radio" value="1" name="estadoRol3" checked="true"></td>
-                         <td><input type="radio" name="estadoRol3" value="0"></td>
-                         @else
-                         <td><input type="radio" value="1" name="estadoRol3"></td>
-                         <td><input type="radio" name="estadoRol3" value="0" checked="true"></td>   
-                         @endif  
-                    </tr>
-                </tbody>
-           </table>
-           <div class="form-group text-center">
-                <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
-                    Guardar Cambios
-                </button>
-             </div>    
-        </form>   
+                <form  method="POST" action="{{  url('usuario/editRol').'/'.$usuario->id  }}">
+                    <input type="hidden" name="_method" value="PUT">
+                    {{ csrf_field() }}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Rol</th>
+                                <th>Activo</th>
+                                <th>Desactivo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Administrador</td>
+                                @if ($rol1 == 1)
+                                <td><input type="radio" value="1" name="estadoRol1" checked="true"></td>
+                                <td><input type="radio" name="estadoRol1" value="0"></td>
+                                @else
+                                <td><input type="radio" value="1" name="estadoRol1"></td>
+                                <td><input type="radio" name="estadoRol1" value="0" checked="true"></td>   
+                                @endif  
+                            </tr>
+                            <tr>
+                                <td>Arrendatario</td>
+                                @if ($rol2 == 1)
+                                <td><input type="radio" value="1" name="estadoRol2" checked="true"></td>
+                                <td><input type="radio" name="estadoRol2" value="0"></td>
+                                @else
+                                <td><input type="radio" value="1" name="estadoRol2"></td>
+                                <td><input type="radio" name="estadoRol2" value="0" checked="true"></td>   
+                                @endif  
+                            </tr>
+                            <tr>
+                                 <td>Propietario</td>
+                                 @if ($rol3 == 1)
+                                 <td><input type="radio" value="1" name="estadoRol3" checked="true"></td>
+                                 <td><input type="radio" name="estadoRol3" value="0"></td>
+                                 @else
+                                 <td><input type="radio" value="1" name="estadoRol3"></td>
+                                 <td><input type="radio" name="estadoRol3" value="0" checked="true"></td>   
+                                 @endif  
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                            Guardar Cambios
+                        </button>
+                    </div>    
+                </form>
 
+            </div> 
+        </div>
     </div> 
     <div class="col-md-2"></div>
 </div>
--- resources/views/importCsv/chooseColumnsCsv.blade.php
+++ resources/views/importCsv/chooseColumnsCsv.blade.php
@@ -6,39 +6,49 @@
         <div class="col-md-2"></div>
         <div class="col-md-8">
             {!!	Notification::showAll()	!!}
-            <form class="form-horizontal" method="POST" action="{{ URL::to('importCsvUsers') }}" enctype="multipart/form-data">
-                {{ csrf_field() }}
+            <div class="panel panel-default">
+				<div class="panel-heading">
+                    Seleccionar columnas del archivo CSV
+                </div>
+				<div class="panel-body">
 
-                <input type = "hidden" name ="file" value = "{{$file}}"></input>
-                <input type = "hidden" name ="origen" value = "{{$origen}}"></input>
+                    <form class="form-horizontal" method="POST" action="{{ URL::to('importCsvUsers') }}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
 
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Campo</th>
-                            <th>Columna del CSV</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($columnsTable as $columnTable)
-                            <tr>
-                                <th>{{$columnTable}}</th>
-                                <td>
-                                    <select name="{{$columnTable}}" size="1">
-                                        @foreach($columns as $i => $column)
-                                            <option value="{{$i}}">{{$column}}</option>
-                                        @endforeach
-                                    </select> 
-                                </td>
-                            </tr>
-                        @endforeach
-                        
-                    </tbody>
-                </table>
-                <br />
-                <br />
-                <button class="btn btn-primary">Importar CSV</button>
-            </form>
+                        <input type = "hidden" name ="file" value = "{{$file}}"></input>
+                        <input type = "hidden" name ="origen" value = "{{$origen}}"></input>
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Campo</th>
+                                    <th>Columna del CSV</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($columnsTable as $columnTable)
+                                    <tr>
+                                        <th>{{$columnTable}}</th>
+                                        <td>
+                                            <select name="{{$columnTable}}" size="1">
+                                                @foreach($columns as $i => $column)
+                                                    <option value="{{$i}}">{{$column}}</option>
+                                                @endforeach
+                                            </select> 
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                
+                            </tbody>
+                        </table>
+                        <br />
+                        <br />
+                        <button class="btn btn-primary">Importar CSV</button>
+                    </form>
+
+                </div>
+            </div>
+
         </div>
         <div class="col-md-2"></div>
     </div>
