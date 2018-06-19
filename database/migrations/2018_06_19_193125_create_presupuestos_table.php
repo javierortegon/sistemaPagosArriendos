c2656645f26f87a310aa2a6af8aa7d3345a32931
David-B-A <dabelloa@unal.edu.co>
2018-06-19 14:36:21 -0500

    Creando migración de tabla presupuestos (primera version)

---
+++ database/migrations/2018_06_19_193125_create_presupuestos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresupuestosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presupuestos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_usuario')->unsigned();
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->integer('propiedad')->unsigned();
+            $table->foreign('propiedad')->references('id')->on('propiedades');
+            $table->decimal('valor_primer_pago', 14, 2);
+            $table->integer('numero_de_cuotas', 14, 2)->unsigned(); // excluyendo la primera
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presupuestos');
+    }
+}
