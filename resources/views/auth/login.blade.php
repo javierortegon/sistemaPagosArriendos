a031ad54cd2546b559f7efa9842ca3dac15a843f
David-B-A <dabelloa@unal.edu.co>
2018-06-29 11:16:16 -0500

    Ajustando ingreso solo con documento (se quita la opción de entrar con email)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-4 control-label">Documento</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="documento" type="text" class="form-control" name="documento" value="{{ old('email') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('documento'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('documento') }}</strong>
                                     </span>
                                 @endif
                             </div>
